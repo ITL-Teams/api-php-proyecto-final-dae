e68d498f528d2cf3bd3b99bdad8555c76eb32695
andresRodBar <andres.rb.arb@gmail.com>
2020-04-24 16:13:41 -0500

    error con controladores y primera vista device-config

---
+++ laravel-passport/resources/views/panel/device-config.blade.php
@@ -0,0 +1,34 @@
+@extends("layout.app")
+
+@section("content")
+
+	<h1>Lista de Dispositivos</h1>
+
+	<div class="container">
+
+		<table class="table table-hover table-condensed table-bordered">
+			<tr>
+				<td>Direcci&oacute;n IP</td>
+				<td>Nombre Dispositivo</td>
+				<td>Seleccionar</td>
+				<td>Acciones</td>
+			</tr>
+
+			<!--@foreach()-->
+
+				<tr>
+					<td></td>
+					<td></td>
+					<td><span class="btn btn-primary btn-sm" id="select">Select</span></td>
+					<td>
+						<span class="btn btn-warning btn-sm" id="edit">Edit</span>
+						<span class="btn btn-danger btn-sm" id="delete">Delete</span>
+					</td>
+				</tr>	
+
+			<!--@endforeach-->
+
+		</table>
+
+	</div>
+@endsection
