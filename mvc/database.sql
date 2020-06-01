cc3941aa5255f86650893e7b1c87ec3198120a96
javier alejandro ortiz gaona <javi_96_360@hotmail.com>
2020-05-31 20:41:06 -0500

    CORRECCION SCRIPT DE BASE DE DATOS

--- mvc/database.sql
+++ mvc/database.sql
@@ -1,129 +1,40 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2020 a las 03:14:48
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+DROP DATABASE IF EXISTS netws;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE netws;
+USE netws;
 
+CREATE TABLE contact ( 
+    id         INT NOT NULL AUTO_INCREMENT,
+    email      VARCHAR(255) NOT NULL,
+    comment    TEXT NOT NULL,
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+    PRIMARY KEY (id)
+);
 
---
--- Base de datos: `netws`
---
-CREATE DATABASE IF NOT EXISTS `netws` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `netws`;
+CREATE TABLE services ( 
+    id           INT NOT NULL AUTO_INCREMENT,
+    type         VARCHAR(10) NOT NULL,
+    privacy      VARCHAR(10) NOT NULL,
+    service_name VARCHAR(255) NOT NULL,
+    description  TEXT NOT NULL,
+    code         TEXT NOT NULL,
+    reference    VARCHAR(40),
 
--- --------------------------------------------------------
+    PRIMARY KEY (id)
+);
 
---
--- Estructura de tabla para la tabla `auth_list`
---
+CREATE TABLE auth_list ( 
+    reference    VARCHAR(40) NOT NULL,
+    token        VARCHAR(20) NOT NULL
+);
+CREATE TABLE users ( 
+    id           INT NOT NULL AUTO_INCREMENT,
+    name         VARCHAR(50) NOT NULL,
+    email      VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    token  VARCHAR(20) NOT NULL,
 
-CREATE TABLE `auth_list` (
-  `reference` varchar(40) NOT NULL,
-  `token` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `contact`
---
-
-CREATE TABLE `contact` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `comment` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `services`
---
-
-CREATE TABLE `services` (
-  `id` int(11) NOT NULL,
-  `type` varchar(10) NOT NULL,
-  `privacy` varchar(10) NOT NULL,
-  `service_name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `code` text NOT NULL,
-  `reference` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` int(5) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `mail` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `token` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `services`
---
-ALTER TABLE `services`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `mail` (`mail`),
-  ADD UNIQUE KEY `token` (`token`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contact`
---
-ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `services`
---
-ALTER TABLE `services`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+    PRIMARY KEY (id),
+	UNIQUE KEY (email,token)
+	
+	);
